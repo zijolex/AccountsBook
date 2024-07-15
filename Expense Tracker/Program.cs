490fb8928586914fb0fd7066ae4c552c8c3aa82c
Alex Mathew <alexmathewthadathil@gmail.com>
2024-07-15 13:45:55 +0530

    My first Commit

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,37 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+//Register Syncfusion license
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCeUx3QXxbf1x0ZFRHal9STnZZUiweQnxTdEFjXn9ZcXVWRWBaUkJ0Ww==");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2U1hhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Udk1iWXpYc3BUQWhf");
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Expense Tracker/obj/Release/net8.0/PubTmp/EFSQLScripts/Expense_Tracker.Models.ApplicationDbContext.sql
@@ -0,0 +1,99 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707091325_Initial Create'
+)
+BEGIN
+    CREATE TABLE [Categories] (
+        [CategoryId] int NOT NULL IDENTITY,
+        [Title] nvarchar(50) NOT NULL,
+        [Icon] nvarchar(5) NOT NULL,
+        [Type] nvarchar(10) NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([CategoryId])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707091325_Initial Create'
+)
+BEGIN
+    CREATE TABLE [Transactions] (
+        [TransactionId] int NOT NULL IDENTITY,
+        [CategoryId] int NOT NULL,
+        [Amount] int NOT NULL,
+        [Note] nvarchar(75) NULL,
+        [Date] datetime2 NOT NULL,
+        CONSTRAINT [PK_Transactions] PRIMARY KEY ([TransactionId]),
+        CONSTRAINT [FK_Transactions_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([CategoryId]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707091325_Initial Create'
+)
+BEGIN
+    CREATE INDEX [IX_Transactions_CategoryId] ON [Transactions] ([CategoryId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707091325_Initial Create'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240707091325_Initial Create', N'8.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707092243_Initial'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240707092243_Initial', N'8.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240707105837_Expense'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240707105837_Expense', N'8.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
